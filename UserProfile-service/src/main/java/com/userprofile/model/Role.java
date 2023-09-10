3cf999afed362a3aa6272e98b8e1a3fc061fcba5
Manmeet121999 <101594580+Manmeet121999@users.noreply.github.com>
2023-09-10 11:27:20 +0530

    Delete UserProfile-service directory

--- UserProfile-service/src/main/java/com/userprofile/model/Role.java
+++
@@ -1,8 +0,0 @@
-package com.userprofile.model;
-
-public interface Role {
-
-	public static final String Customer = "ROLE_Customer";
-	public static final String merchant = "ROLE_Merchant";
-	public static final String deliveryAgent = "ROLE_DeliveryAgent";
-}
